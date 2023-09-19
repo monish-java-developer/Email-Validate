d9c7ff4f9398508a0fc499af1a9a86d06f45de26
monish-java-developer <monish.java.developer@gmail.com>
2023-09-19 21:48:42 +0530

    email

---
+++ Email-Validator/js/index.js
@@ -0,0 +1,39 @@
+let result = {
+
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "monish07.ms",
+    "email": "monish07.ms@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+
+}
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_pu8kyZZhiMbDDIdV6u3X5tCCzEIx8InwZI3o4fCC"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
